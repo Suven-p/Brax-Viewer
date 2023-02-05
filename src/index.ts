3288c696b01c9293c5f4cd4d6c7f7765e44276b9
Suven-p <pandeysuven@gmail.com>
2023-02-05 21:09:07 +0545

    Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 
 import { Viewer } from './viewer';
 import system from './braxConfig.json';
-import { isJsxClosingFragment } from 'typescript';
 
 declare global {
     interface Window {
@@ -28,13 +27,7 @@ window.system = system;
 //         });
 //     }
 // });
-const a = system.pos.map((timestep, i1) => {
-    return timestep.map((body, i2) => {
-        return [0.0, 0.0, 0.0];
-    });
-});
-console.log(a);
-system.pos = a;
+
 
 system.config.bodies.push({
     "name": "Target",
@@ -77,6 +70,7 @@ system.config.bodies.push({
     },
     "mass": 10.0
 });
+
 system.pos[0].push([2, 0, 0.05]);
 system.rot[0].push([1, 0, 0, 0]);
 var viewer = new Viewer(domElement, system);
