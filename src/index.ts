467bb6c0a6e7e45de140150a521e4ce11ab79f1b
Suven-p <pandeysuven@gmail.com>
2023-02-07 16:30:36 +0545

    Try to load data from mujoco

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 // @ts-nocheck
 
 import { Viewer } from './viewer';
-import system from './braxConfig.json';
+import system from './braxConfig2.json';
+import { mocapMotion } from '../dataset/mocapmotion';
 
 declare global {
     interface Window {
         system: any;
+        mocapMotion: any;
     }
 }
 
 const domElement = document.getElementById('brax-viewer');
 window.system = system;
+window.mocapMotion0 = mocapMotion;
+window.mocapMotion0.forEach((motion) => {
+    // motion.splice(0, 1);
+    motion.splice(0, 1);
+    motion.splice(19, 1);
+});
+system.pos = window.mocapMotion0;
+// for (let i = 0; i < mocapMotion0.length - 1; i++) {
+//     system.rot.push(system.rot[0]);
+// }
 
 // system.config.bodies.forEach(body => {
 //     if (body.colliders && body.colliders.length > 0) {
@@ -29,48 +41,48 @@ window.system = system;
 // });
 
 
-system.config.bodies.push({
-    "name": "Target",
-    "colliders": [
-        {
-            "position": {
-                "x": 2.0,
-                "y": 0.0,
-                "z": 0.05
-            },
-            "sphere": {
-                "radius": 0.1
-            },
-            "material": {
-                "friction": 1.0,
-                "elasticity": 0.0
-            },
-            "color": "",
-            "hidden": false,
-            "noContact": false
-        }
-    ],
-    "inertia": {
-        "x": 1.0,
-        "y": 1.0,
-        "z": 1.0
-    },
-    "frozen": {
-        "position": {
-            "x": 1.0,
-            "y": 1.0,
-            "z": 1.0
-        },
-        "rotation": {
-            "x": 1.0,
-            "y": 1.0,
-            "z": 1.0
-        },
-        "all": true
-    },
-    "mass": 10.0
-});
+// system.config.bodies.push({
+//     "name": "Target",
+//     "colliders": [
+//         {
+//             "position": {
+//                 "x": 2.0,
+//                 "y": 0.0,
+//                 "z": 0.05
+//             },
+//             "sphere": {
+//                 "radius": 0.1
+//             },
+//             "material": {
+//                 "friction": 1.0,
+//                 "elasticity": 0.0
+//             },
+//             "color": "",
+//             "hidden": false,
+//             "noContact": false
+//         }
+//     ],
+//     "inertia": {
+//         "x": 1.0,
+//         "y": 1.0,
+//         "z": 1.0
+//     },
+//     "frozen": {
+//         "position": {
+//             "x": 1.0,
+//             "y": 1.0,
+//             "z": 1.0
+//         },
+//         "rotation": {
+//             "x": 1.0,
+//             "y": 1.0,
+//             "z": 1.0
+//         },
+//         "all": true
+//     },
+//     "mass": 10.0
+// });
 
-system.pos[0].push([2, 0, 0.05]);
-system.rot[0].push([1, 0, 0, 0]);
+// system.pos[0].push([2, 0, 0.05]);
+// system.rot[0].push([1, 0, 0, 0]);
 var viewer = new Viewer(domElement, system);
