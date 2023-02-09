3a017f56dac3a1d876845e853c177e8de94335b9
Suven-p <pandeysuven@gmail.com>
2023-02-10 00:00:19 +0545

    Update for dataset from start of clip

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import system from './braxConfig2.json';
 import xipos from '../dataset/xipos.json';
 import ximat from '../dataset/ximat.json';
 import { mocapMotion } from '../dataset/mocapmotion';
+import initialxipos from '../dataset/init_xipos.json';
+import initialximat from '../dataset/init_ximat.json';
 
 declare global {
     interface Window {
@@ -13,6 +15,7 @@ declare global {
         mocapMotion: any;
         xipos: any;
         ximat: any;
+        initialximat: any;
     }
 }
 
@@ -34,15 +37,58 @@ window.ximat = ximat.ximat.map((motion) => {
     return motion.map(body => {
         const m = new THREE.Matrix4();
         m.fromArray([body[0], body[1], body[2], 0, body[3], body[4], body[5], 0, body[6], body[7], body[8], 0, 0, 0, 0, 1]);
+        const e = new THREE.Euler();
+        e.setFromRotationMatrix(m);
+        e.x += 45;
         const q = new THREE.Quaternion();
         q.setFromRotationMatrix(m);
         return [q.w, q.x, q.y, q.z];
     });
 });
+window.initialxipos = window.xipos[0].map(body => {
+    return { x: body[0], y: body[1], z: body[2] };
+});
+window.initialxipos.splice(1, 1);
+window.initialxipos.splice(19, 1);
+window.initialximat = window.ximat[0].map(body => {
+    const m = new THREE.Matrix4();
+    m.fromArray([body[0], body[1], body[2], 0, body[3], body[4], body[5], 0, body[6], body[7], body[8], 0, 0, 0, 0, 1]);
+    const e = new THREE.Euler();
+    e.setFromRotationMatrix(m);
+    return { x: e.x, y: e.y, z: e.z };
+});
+window.initialximat.splice(1, 1);
+window.initialximat.splice(19, 1);
+
 system.pos = window.xipos;
 system.rot = window.ximat;
+// system.pos = [window.xipos[window.xipos.length - 1]];
+// system.rot = [window.ximat[window.xipos.length - 1]];
 
-
+system.config.bodies.forEach((body, i) => {
+    if (i === 0) return;
+    body.colliders.forEach((collider) => {
+        if (!collider || !collider.position) return;
+        collider.position = { x: 0.0, y: 0.0, z: 0.0 };
+        collider.rotation = { x: 0.0, y: 0.0, z: 0.0 };
+        const radToDeg = THREE.MathUtils.radToDeg;
+        if (body.name === 'head') {
+            collider.position.y = -0.1;
+        }
+        if (body.name === 'lclavicle') {
+            collider.position = {
+                "x": 0.08,
+                "y": -0.3,
+                "z": 0.2
+            };
+            collider.rotation = {
+                "x": -75,
+                "y": -68,
+                "z": -100
+            };
+        }
+    });
+});
 
 
 // system.config.bodies.push({
